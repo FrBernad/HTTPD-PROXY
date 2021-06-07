ea28fbfa25a5b78ce32febfd7914682218451233
Francisco <frbernad@itba.edu.ar>
2021-06-07 13:04:28 -0300

    feat: add base64 lib

---
+++ tests/base64_test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "../status_line_parser.h"
+
+int main(int argc, char const *argv[]) {
+    char *response = "HTTP/1.1 301 Moved Permanently\r\n";
+
+    return 0;
+}
