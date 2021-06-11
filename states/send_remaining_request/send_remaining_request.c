628b6583e4545fd79e31ff168dc2b088973c1f6f
Nicolás Rampoldi <nrampoldi@itba.edu.ar>
2021-06-11 11:56:12 -0300

    fix: delete obsolete files

--- states/send_remaining_request/send_remaining_request.c
+++
@@ -1,8 +0,0 @@
-#include "send_remaning_request.h"
-
-void send_remaining_request_on_arrival(const unsigned state, struct selector_key *key);
-
-unsigned
-send_remaining_request_on_write_ready(struct selector_key *key);
-
-#endif
